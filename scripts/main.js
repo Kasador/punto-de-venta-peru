bf3963977f4457504d26c705b73bbe26b9d2e90d
Kasador <hunterstevenshaw@gmail.com>
2024-10-29 13:07:22 -0500

    First commit!

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Test clock
+// let counter = 0;
+// setInterval(() => {
+//     console.log(`Update every second: ${counter++}`);
+// }, 1000);
+
+class Main {
+    constructor() {
+        this.counter = 0;
+    }
+
+    
+}
+
+(() => {
+    // Connect JS
+    console.log('JS connected!');
+    const main = new Main();
+})();
